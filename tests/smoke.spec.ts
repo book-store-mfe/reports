1771c36d3beb12d2340b2eb5597a8b1d7a06fb16
Jeferson Belmiro <jeferson.belmiro@cwi.com.br>
2025-07-13 23:20:07 -0300

    fix: unit test

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-import { describe } from 'node:test';
 
 const baseUrl = 'https://book-store-mfe.github.io/store/#/';
 
@@ -34,13 +33,22 @@ test.describe('Store MFE smoke', () => {
 
     test('should render catalog', async ({ page }) => {
       await page.getByRole('button', { name: 'catalog' }).click();
+      await page.waitForLoadState('networkidle');
+
       await expect(page.locator('mat-card')).toHaveCount(4);
+
+      const firstBook = page.locator('mat-card').first()
+      const title = await firstBook.locator('mat-card-title').textContent();
+      await firstBook.locator('button').click();
+      await page.waitForLoadState('networkidle');
+
+      await expect(page.locator('app-book-review-dialog h2')).toHaveText(`Review de ${title}`);
     });
 
     test('should render account', async ({ page }) => {
       await page.getByRole('button', { name: 'account' }).click();
 
-      await expect(page.locator('input[formcontrolname="name"]')).toHaveValue('Jhonx');
+      await expect(page.locator('input[formcontrolname="name"]')).toHaveValue('Jhon');
       await expect(page.locator('input[formcontrolname="email"]')).toHaveValue('jhon@gmail.com');
     });
 
